2a4ec6c2664cd1a46e802f1d004ba9875c9db460
La1x <laix.moscow@gmail.com>
2017-11-14 20:54:55 +0300

    Add Point and Piston classes

---
+++ src/laix/robotsegments/model/Piston.java
@@ -0,0 +1,47 @@
+package laix.robotsegments.model;
+
+import javafx.scene.paint.Color;
+
+public class Piston extends Point {
+    private double width;
+    private double height;
+    private Color color;
+
+    public Piston(double x, double y, double width, double height, Color color) {
+        super(x, y);
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    public Piston(double x, double y, double width, double height) {
+        super(x, y);
+        this.width = width;
+        this.height = height;
+        this.color = Color.DARKBLUE;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+}
